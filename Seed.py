0c89720ddf35565729457560d12717eae263c176
MiriamRehema <rehemamiriam30@gmail.com>
2023-09-08 11:26:55 +0300

    seed.py

--- Seed.py
+++ Seed.py
@@ -15,22 +15,8 @@ def seed_database():
     initial_data = [
         {"account": "Miriam", "password": "659556"},
         {"account": "Rehema", "password": "fur677"},
-        {"account": "Irungu", "password": "jhfyk6545"},
-        {"account": "Hellen", "password": "659556"},
-        {"account": "Jane", "password": "fur677"},
-        {"account": "Hadassah", "password": "jhfyk6545"},
-        {"account": "James", "password": "659556"},
-        {"account": "Wilson", "password": "fur677"},
-        {"account": "Ruth", "password": "jhfyk6545"},
-        {"account": "Joseph", "password": "659556"},
-        {"account": "Malkia", "password": "fur677"},
-        {"account": "Evan", "password": "jhfyk6545"},
-        {"account": "Westly", "password": "659556"},
-        {"account": "George", "password": "fur677"},
-        {"account": "Cory", "password": "jhfyk6545"},
-        {"account": "Duke", "password": "659556"},
-        {"account": "Mary", "password": "fur677"},
-        {"account": "Houston", "password": "jhfyk6545"},
+        {"account": "Irungu", "password": "jhfyk6545"}
+       
     ]
 
     # Insert the initial data into the database
