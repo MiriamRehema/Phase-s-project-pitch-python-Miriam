a005b2dc9a367c31b21e7eee671f873cf2e93edf
MiriamRehema <rehemamiriam30@gmail.com>
2023-09-08 10:10:30 +0300

    seed.py

--- Seed.py
+++ Seed.py
@@ -16,6 +16,21 @@ def seed_database():
         {"account": "Miriam", "password": "659556"},
         {"account": "Rehema", "password": "fur677"},
         {"account": "Irungu", "password": "jhfyk6545"},
+        {"account": "Hellen", "password": "659556"},
+        {"account": "Jane", "password": "fur677"},
+        {"account": "Hadassah", "password": "jhfyk6545"},
+        {"account": "James", "password": "659556"},
+        {"account": "Wilson", "password": "fur677"},
+        {"account": "Ruth", "password": "jhfyk6545"},
+        {"account": "Joseph", "password": "659556"},
+        {"account": "Malkia", "password": "fur677"},
+        {"account": "Evan", "password": "jhfyk6545"},
+        {"account": "Westly", "password": "659556"},
+        {"account": "George", "password": "fur677"},
+        {"account": "Cory", "password": "jhfyk6545"},
+        {"account": "Duke", "password": "659556"},
+        {"account": "Mary", "password": "fur677"},
+        {"account": "Houston", "password": "jhfyk6545"},
     ]
 
     # Insert the initial data into the database
@@ -26,6 +41,7 @@ def seed_database():
     # Commit the changes and close the session
     session.commit()
     session.close()
+    
 
 if __name__ == "__main__":
     # Call the seed_database function to insert the initial data
